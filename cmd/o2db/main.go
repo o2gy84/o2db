bdbfda2ee8a4c89eea987c96f234c3ad40e8b76b
Victor Mogilin <o2gy84@gmail.com>
2020-02-09 09:15:41 -0500

    add logrus

--- cmd/o2db/main.go
+++ cmd/o2db/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
 	"github.com/o2gy84/o2db/pkg/lsm"
+	"github.com/o2gy84/o2db/pkg/commit"
+
 )
 
 func main() {
+
+	log.Printf("[commit] %s", commit.Commit)
 
 	var lsm lsm.LSM
 	fmt.Printf("lsm is: %s\n", lsm)
