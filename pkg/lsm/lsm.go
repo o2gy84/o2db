aeb4038742d1792f80cb5093d51a6270fd39e206
Victor Mogilin <o2gy84@gmail.com>
2020-02-09 09:06:35 -0500

    removed o2db.go

--- pkg/lsm/lsm.go
+++ pkg/lsm/lsm.go
@@ -6,8 +6,9 @@ import (
 
 type LSM struct {
 	A int64
+	B int64
 }
 
 func (lsm LSM) String() string {
-	return fmt.Sprintf("TEST XXX: %d", lsm.A)
+	return fmt.Sprintf("TEST XXX: %d, YYY: %d", lsm.A, lsm.B)
 }
